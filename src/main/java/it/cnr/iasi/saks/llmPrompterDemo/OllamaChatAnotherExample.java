5c23f0f9c012d46ee46a66444f8c63bea976564a
Gulyx <guglielmo.deangelis@iasi.cnr.it>
2024-11-13 14:31:51 +0100

    modified the example according to the suggetions

--- src/main/java/it/cnr/iasi/saks/llmPrompterDemo/OllamaChatAnotherExample.java
+++ src/main/java/it/cnr/iasi/saks/llmPrompterDemo/OllamaChatAnotherExample.java
@@ -11,27 +11,30 @@ public class OllamaChatAnotherExample {
   public static void main(String[] args) {
     // The model name to use (e.g., "orca-mini", "mistral", "llama2", "codellama", "phi", or
     // "tinyllama")
-    String modelName = "llama";
-    // String modelVersion = "latest";
-    String modelVersion = "3.1";
+    // String modelName = "llama";
+    // String modelName = "llama3.1";
+    String modelName = "llama2";
+    String modelVersion = "latest";
+    // String modelVersion = "3.1";
     int ollamaPort = 11434;
 
     // Create and start the Ollama container
     GenericContainer<?> ollama = new GenericContainer<>("langchain4j/ollama-" + modelName + ":" + modelVersion)
             .withExposedPorts(ollamaPort);
 
-    ollama.start();
+        ollama.start();
 
     // Build the ChatLanguageModel
     ChatLanguageModel model =
         OllamaChatModel.builder().baseUrl(baseUrl(ollama)).modelName(modelName).build();
 
     // Example usage
-    String answer = model.generate("Provide 3 short bullet points explaining why Java is awesome");
+    //    String answer = model.generate("Provide 3 short bullet points explaining why Java is awesome");
+    String answer = model.generate("What is your Name?");
     System.out.println(answer);
 
     // Stop the Ollama container
-    ollama.stop();
+        ollama.stop();
   }
 
   private static String baseUrl(GenericContainer<?> ollama) {
--- src/main/java/it/cnr/iasi/saks/llmPrompterDemo/OllamaChatExample.java
+++ src/main/java/it/cnr/iasi/saks/llmPrompterDemo/OllamaChatExample.java
@@ -1,36 +1,49 @@
 package it.cnr.iasi.saks.llmPrompterDemo;
 
-// import dev.langchain4j.model.chat.ChatLanguageModel;
-// import dev.langchain4j.model.ollama.OllamaChatModel;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.ollama.OllamaChatModel;
 // import org.testcontainers.ollama.OllamaContainer;
 // import org.testcontainers.utility.DockerImageName;
 
+import java.time.Duration;
+    
 public class OllamaChatExample {
 
-//   public static void main(String[] args) {
-//     // The model name to use (e.g., "orca-mini", "mistral", "llama2", "codellama", "phi", or
-//     // "tinyllama")
-//     String modelName = "llama";
-//     // String modelVersion = "latest";
-//     String modelVersion = "3.1";
+   public static void main(String[] args) {
+     // The model name to use (e.g., "orca-mini", "mistral", "llama2", "codellama", "phi", or
+     // "tinyllama")
+     String modelName = "llama3.1";
+     String modelVersion = "latest";
+     // String modelVersion = "3.1";
 // 
 //     // Create and start the Ollama container
 //     OllamaContainer ollama =
 //         new OllamaContainer(DockerImageName.parse("langchain4j/ollama-" + modelName + ":" + modelVersion)
 //             .asCompatibleSubstituteFor("ollama/ollama"));
+
 //     ollama.start();
 // 
 //     // Build the ChatLanguageModel
 //     ChatLanguageModel model =
 //         OllamaChatModel.builder().baseUrl(baseUrl(ollama)).modelName(modelName).build();
+
+OllamaChatModel model = OllamaChatModel.builder()
+    .baseUrl("http://localhost:11434")
+    .modelName(modelName)
+    .temperature(0.8)
+    .timeout(Duration.ofSeconds(300))
+    .build();
+     
+
 // 
 //     // Example usage
 //     String answer = model.generate("Provide 3 short bullet points explaining why Java is awesome");
-//     System.out.println(answer);
+     String answer = model.generate("What is your Name?");
+     System.out.println(answer);
 // 
 //     // Stop the Ollama container
 //     ollama.stop();
-//   }
+   }
 
 //   private static String baseUrl(GenericContainer<?> ollama) {
 //     return String.format("http://%s:%d", ollama.getHost(), ollama.getFirstMappedPort());
