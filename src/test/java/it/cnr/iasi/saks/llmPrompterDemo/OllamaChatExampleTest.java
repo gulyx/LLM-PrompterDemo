308f41be24035344ad0fa818e6703ab4cc43b485
Gulyx <guglielmo.deangelis@iasi.cnr.it>
2025-05-15 17:00:17 +0200

    Added licence header

--- src/test/java/it/cnr/iasi/saks/llmPrompterDemo/OllamaChatExampleTest.java
+++ src/test/java/it/cnr/iasi/saks/llmPrompterDemo/OllamaChatExampleTest.java
@@ -1,3 +1,20 @@
+/* 
+ * This file is part of the LLM-PrompterDemo project.
+ * 
+ * LLM-PrompterDemo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * LLM-PrompterDemo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with LLM-PrompterDemo.  If not, see <https://www.gnu.org/licenses/>
+ *
+ */
 package it.cnr.iasi.saks.llmPrompterDemo;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
