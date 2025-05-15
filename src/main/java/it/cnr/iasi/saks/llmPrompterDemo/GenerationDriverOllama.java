6f00374a9953b25ece7542d174700e2d82d50eaf
Gulyx <guglielmo.deangelis@iasi.cnr.it>
2025-05-15 16:56:19 +0200

    Updated the current Driver so that to test the new prompter

--- src/main/java/it/cnr/iasi/saks/llmPrompterDemo/GenerationDriverOllama.java
+++ src/main/java/it/cnr/iasi/saks/llmPrompterDemo/GenerationDriverOllama.java
@@ -6,6 +6,7 @@ import java.time.Duration;
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.ollama.OllamaChatModel;
 import it.cnr.iasi.saks.llmPrompter.Prompter;
+import it.cnr.iasi.saks.llmPrompter.impl.DescriptionExamplesPrompter;
 import it.cnr.iasi.saks.llmPrompter.impl.DescriptionOnlyPrompter;
 
 public class GenerationDriverOllama {
@@ -24,7 +25,8 @@ public class GenerationDriverOllama {
                 .build();
 	    String[] items = {"1382", "2375", "1072", "3324", "1641"};
 	    for (String id : items) {
-			Prompter p = new DescriptionOnlyPrompter(id, 5, llm);
+//			Prompter p = new DescriptionOnlyPrompter(id, 5, llm);
+			Prompter p = new DescriptionExamplesPrompter(id, 5, llm);
 			
 			String prompt = p.composePrompt();
 			System.out.println("Querying LLM for id:" + id + " ...");
