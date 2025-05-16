19bf399f6fd42fba96b0d90e7092f3d36014c656
Gulyx <guglielmo.deangelis@iasi.cnr.it>
2025-05-16 15:20:53 +0200

    Improved prompts wrt to the target package of the generated classes

--- src/main/java/it/cnr/iasi/saks/llmPrompter/impl/DescriptionExamplesPrompter.java
+++ src/main/java/it/cnr/iasi/saks/llmPrompter/impl/DescriptionExamplesPrompter.java
@@ -54,7 +54,7 @@ public class DescriptionExamplesPrompter extends Prompter {
 		// https://github.com/langchain4j/langchain4j-examples/blob/8c6870202e7c9be333ec50e04397042bd65d5d69/tutorials/src/main/java/_03_PromptTemplate.java#L28
 		String testFileName = this.problemID + "_" + SUFFIX;
 		String prompt = "As a professional Software Tester, generate a complete Junit class test file (named "
-				+ testFileName + " in the package " + TARGET_PACKAGE + ") with " + this.numberOfTests
+				+ testFileName + ", and declare it as belonging to the package " + TARGET_PACKAGE + ") with " + this.numberOfTests
 				+ " different JUnit tests for the following description in natural language with examples. Specifically below the description, each example is introduced by the keyword "
 				+ TAG_EXAMPLE
 				+ " (where XX is a number), and it is composed by an input tuple and an expected output. Give back only the Junit test class, no other informations.  Here is the description: ";
