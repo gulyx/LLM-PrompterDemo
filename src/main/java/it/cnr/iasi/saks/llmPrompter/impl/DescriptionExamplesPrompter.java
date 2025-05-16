32c9161fa1789782a4fd5d99ad2c0b1b95213c86
Gulyx <guglielmo.deangelis@iasi.cnr.it>
2025-05-16 13:49:39 +0000

    Modified the Prompt. Hopefully it is improved

--- src/main/java/it/cnr/iasi/saks/llmPrompter/impl/DescriptionExamplesPrompter.java
+++ src/main/java/it/cnr/iasi/saks/llmPrompter/impl/DescriptionExamplesPrompter.java
@@ -53,11 +53,11 @@ public class DescriptionExamplesPrompter extends Prompter {
 		// You may improve this by looking at this example:
 		// https://github.com/langchain4j/langchain4j-examples/blob/8c6870202e7c9be333ec50e04397042bd65d5d69/tutorials/src/main/java/_03_PromptTemplate.java#L28
 		String testFileName = this.problemID + "_" + SUFFIX;
-		String prompt = "As a professional Software Tester, generate a complete Junit class test file (named "
-				+ testFileName + ", and declare it as belonging to the package " + TARGET_PACKAGE + ") with " + this.numberOfTests
+		String prompt = "As a professional Software Tester, generate a complete Junit class test file (name the class as :'"
+				+ testFileName + "', and use as first line the declaration: 'package " + TARGET_PACKAGE + "') with " + this.numberOfTests
 				+ " different JUnit tests for the following description in natural language with examples. Specifically below the description, each example is introduced by the keyword "
 				+ TAG_EXAMPLE
-				+ " (where XX is a number), and it is composed by an input tuple and an expected output. Give back only the Junit test class, no other informations.  Here is the description: ";
+				+ " (where XX is a number), and it is composed by an input tuple and an expected output. Emit only the Java code, without any other tag or text.  Here is the description: ";
 
 		prompt = prompt.concat("\n" + this.description);
 
