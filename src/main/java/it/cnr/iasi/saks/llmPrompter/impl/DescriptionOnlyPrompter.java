038021a0921e4a5a2e2ef99b3392d518805a1923
Gulyx <guglielmo.deangelis@iasi.cnr.it>
2025-05-21 15:35:56 +0000

    MOdified prompt: added the keyword "Test" to the name of the class to be genarated

--- src/main/java/it/cnr/iasi/saks/llmPrompter/impl/DescriptionOnlyPrompter.java
+++ src/main/java/it/cnr/iasi/saks/llmPrompter/impl/DescriptionOnlyPrompter.java
@@ -51,7 +51,7 @@ public class DescriptionOnlyPrompter extends Prompter {
 		// https://github.com/langchain4j/langchain4j-examples/blob/8c6870202e7c9be333ec50e04397042bd65d5d69/tutorials/src/main/java/_03_PromptTemplate.java#L28
 		String testFileName = this.problemID + "_" + SUFFIX;
 		String prompt = "As a professional Software Tester, generate a complete Junit class test file (name the class as :'"
-				+ testFileName + "', and use as first line the declaration: 'package " + TARGET_PACKAGE + "') with " + this.numberOfTests
+				+ testFileName + "Test', and use as first line the declaration: 'package " + TARGET_PACKAGE + "') with " + this.numberOfTests
 				+ " different JUnit tests for the following description in natural language. Emit only the Java code, without any other tag or text. Here is the description: ";
 
 		prompt = prompt.concat("\n" + this.description);
