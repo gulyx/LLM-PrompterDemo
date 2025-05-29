4b1ce02f338f630c28880da8fd2413878bf31243
Gulyx <guglielmo.deangelis@iasi.cnr.it>
2025-05-29 14:19:32 +0200

    Addedd propters that emit a java class for the given problem

---
+++ src/main/java/it/cnr/iasi/saks/llmPrompter/impl/DescriptionOnlyJavaPrompter.java
@@ -0,0 +1,71 @@
+/* 
+ * This file is part of the LLM-PrompterDemo project.
+ * 
+ * LLM-PrompterDemo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * LLM-PrompterDemo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with LLM-PrompterDemo.  If not, see <https://www.gnu.org/licenses/>
+ *
+ */
+package it.cnr.iasi.saks.llmPrompter.impl;
+
+import java.io.IOException;
+
+import dev.langchain4j.model.chat.ChatModel;
+import it.cnr.iasi.saks.llmPrompter.Prompter;
+
+public class DescriptionOnlyJavaPrompter extends Prompter {
+
+	public static final String SUFFIX = "DescriptionOnly_java";
+
+	public DescriptionOnlyJavaPrompter(String problemID) {
+		super(problemID);
+
+		this.loadDescription();
+	}
+
+	public DescriptionOnlyJavaPrompter(String problemID, int numberOfTests) {
+		super(problemID, numberOfTests);
+
+		this.loadDescription();
+	}
+
+	public DescriptionOnlyJavaPrompter(String problemID, int numberOfTests, ChatModel llm) {
+		super(problemID, numberOfTests, llm);
+
+		this.loadDescription();
+	}
+
+	@Override
+	public String composePrompt() {
+
+		// You may improve this by looking at this example:
+		// https://github.com/langchain4j/langchain4j-examples/blob/8c6870202e7c9be333ec50e04397042bd65d5d69/tutorials/src/main/java/_03_PromptTemplate.java#L28
+		String javaFileName = this.problemID + "_" + SUFFIX;
+		String prompt = "As a professional Software Engineer, generate a complete Java class file (name the class as :'"
+				+ javaFileName + "', and use as first line the declaration: 'package " + TARGET_PACKAGE + "') for the following description in natural language. Emit only the Java code, without any other tag or text. Here is the description: ";
+
+		prompt = prompt.concat("\n" + this.description);
+
+		return prompt;
+	}
+
+	@Override
+	public void savePrompt(String prompt) throws IOException {
+		this.savePrompt(prompt, SUFFIX);
+	}
+
+	@Override
+	public void saveCurrentResponse() throws IOException {
+		this.saveCurrentResponse(SUFFIX);
+	}
+
+}
